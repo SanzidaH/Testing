85a92ab3958be7457124f67ff5a7737ba146a47a
sanzida <konahoque@gmail.com>
2021-04-18 10:09:41 +0600

    commiting from branch

--- cdf.py
+++ cdf.py
@@ -5,8 +5,6 @@ import os
 
 ### Plot graph ###
 numbers = [5,10,15,20,25]
-#numbers = [5, 10]
-
 
 for val in numbers:
  cnt = 0
@@ -35,22 +33,18 @@ for val in numbers:
  plt.ylabel('Probability (at or below delay)')
  plt.title('Empirical CDF of packet delay')
  
- #plt.plot(x, y, label = str(val))
- #plt.plot(np.sort(a), np.linspace(0, 100, len(a), endpoint=False), label = str(val))
-#2
+
  X2 = np.sort(a)
  F2 = np.array(range(len(X2)))/float(len(X2))
  
- plt.plot(X2, F2, label = "Consumer: " + str(val))
+ plt.plot(X2, F2, label = "Consumer No: " + str(val))
  plt.legend(loc='best')
 
-#3
-#plt.plot(np.sort(a), np.linspace(0, 1, len(a), endpoint=False))
 
 plt.savefig("new_cumulative_density_distribution_consumer.png", bbox_inches='tight')
 fig = plt.gcf()
 fig.set_size_inches((10, 3), forward=False)
-#fig.savefig("new_cumulative_density_distribution_consumers"+str(val)+".png", bbox_inches='tight') #, dpi=500
+
 plt.show()
 plt.close()
 
